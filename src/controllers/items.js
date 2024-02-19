f1e11d2fd3bdd5dbfb7b820574ed0d42ce52a5f3
Zeth Kenneth Pungtod <zeth.pungtod@citysavings.com.ph>
2024-02-19 16:12:24 +0800

    commit

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -11,4 +11,14 @@ router.get('/', (res, req) => {
     return req.status(200).send("THIS IS GET ITEMS")
 })
 
+router.get("/item", (res, req) => {
+  logger.log({
+    timestamp: new Date().toDateString(),
+    label: "server",
+    level: "info",
+    message: `GET API ITEMS`,
+  });
+  return req.status(200).send("THIS IS GET ITEMS");
+});
+
 module.exports = router;
