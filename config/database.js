c5168e66c0158c1b72a6e4551906abcac05ced20
Zeth Kenneth Pungtod <zeth.pungtod@citysavings.com.ph>
2024-03-01 22:50:32 +0800

    done config database

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ const pg = new Pool({
   host: "localhost",
   user: "postgres",
   port: 5432,
-  password: "password",
-  database: "IMTIAZ"
+  password: "password123",
+  database: "imtiaz"
 });
 
 // // pg.connect();
@@ -20,3 +20,45 @@ const pg = new Pool({
 // })
 
 module.exports = pg;
+
+/*
+ --CATABASE
+ CREATE DATABASE IMTIAZ;
+ 
+ --Users
+ CREATE TABLE users(
+    userId SMALLSERIAL ,
+    name VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(50),
+    PRIMARY KEY(userId)
+ );
+
+ --Items 
+ CREATE TABLE items(
+    itemId SMALLSERIAL,
+    typeOfProduct VARCHAR(10),
+    description VARCHAR(100),
+    inStock INTEGER,
+    lastIn DATE ,
+    lastOut DATE ,
+    Price  FLOAT,
+    PRIMARY KEY(itemId)
+ );
+ 
+ --Sales
+ Create TABLE sales(
+    saleId SMALLSERIAL UNIQUE,
+    quantity INTEGER,
+    itemId INTEGER,
+    userId INTEGER,
+    paymentStatus VARCHAR(10),
+    dateOfSold DATE,
+    dataOfPayed DATE,
+    FOREIGN KEY(itemId) REFERENCES items(itemId),
+    FOREIGN KEY(userId) REFERENCES users(userId)
+ );
+
+INSERT INTO users (name, username, password) VALUES ('bob', 'bob123', 'bob123'), ('ana', 'ana1999', 'anabeauty');
+
+*/ 
